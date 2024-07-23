9693e9eb500b6240aac7c99e3d2fc388723e8630
linsir <student_yxl@126.com>
2024-07-23 10:26:46 +0800

    初始化1.0.0

---
+++ src/main/java/com/linsir/redis/config/RedisTemplateConfiguration.java
@@ -0,0 +1,36 @@
+package com.linsir.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author linsir
+ * @version 1.0.0
+ * @title RedisTemplateConfiguration
+ * @description
+ * @create 2024/7/23 10:23
+ */
+
+@Configuration
+public class RedisTemplateConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory)    {
+        RedisTemplate<String, Object> redisTemplate= new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+
+        //设置通用序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+
+}
